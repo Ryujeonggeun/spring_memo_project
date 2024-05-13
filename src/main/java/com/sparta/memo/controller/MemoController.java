69841404ccc6ad42a1a9b4de42fda01c0e49a552
Ryujeonggeun <161789810+Ryujeonggeun@users.noreply.github.com>
2024-05-13 21:53:05 +0900

    Update,Delete 구현

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -12,13 +12,14 @@ import java.util.*;
 public class MemoController {
 
     private final Map<Long, Memo> memoList = new HashMap<>();
+
     @PostMapping
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
-       // RequestDto -> Entity
+        // RequestDto -> Entity
         Memo memo = new Memo(requestDto);
 
         // Memo Max ID Check
-        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1 ; //중복 방지
+        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1; //중복 방지
         memo.setId(maxId);
 
         //DB 저장
@@ -34,6 +35,33 @@ public class MemoController {
         //Map To List
         List<MemoResponseDto> responseList = memoList.values().stream()
                 .map(MemoResponseDto::new).toList();
-          return responseList;
+        return responseList;
+    }
+
+    @PutMapping("/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        //해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            //해당 메모 가져오기
+            Memo memo = memoList.get(id);
+
+            // memo 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        //해당 메모가 DB에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            //해당 메모를 삭제하기
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
     }
 }
